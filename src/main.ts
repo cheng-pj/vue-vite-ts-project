3b7a1c0ce22cb44099d34ac91832788bf9eaa9fd
Mr.Cheng <2895038949@qq.com>
2022-01-21 18:03:14 +0800

    none

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,27 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import {
+	// create naive ui
+	create,
+	// component
+	NButton
+} from 'naive-ui'
+
+// 通用字体
+import 'vfonts/Lato.css'
+// 等宽字体
+import 'vfonts/FiraCode.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+
+const naive = create({
+	components: [NButton]
+})
+app.use(naive)
 
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,67 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>{{ state.count }}</h1>
+		<div style="display: flex;">
+			<button class="btn" @click="onClick">点击增加</button>
+			<button class="btn" @click="onReset">重置</button>
+		</div>
+	</div>
 </template>
+<script>
+import {reactive} from 'vue'
 
-<style>
+export default {
+
+	setup() {
+		const state = reactive({count: 0})
+
+		const onClick = (val) => {
+			console.log('点击', val)
+			state.count++
+		}
+
+		const onReset = () => {
+
+		}
+
+
+		return {state, onClick, onReset}
+	}
+}
+</script>
+<style lang="scss">
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+	.about {
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+	}
+}
+
+.about {
+	background-color: #FFD07F;
+	border-radius: 12px;
+	padding: 10px;
+	color: #333333;
+
+	.btn {
+		padding: 5px 10px;
+		background-color: #FF8243;
+		border-radius: 4px;
+		margin-right: 5px;
+		cursor: pointer;
+		border: 0px solid transparent;
+		color: #ffffff;
+		transition: all 0.3s ease;
+		border
+
+		&:hover {
+			background-color: #FDA65D;
+		}
+
+		&:focus {
+			border: 2px solid rgba(255, 130, 67, 0.5);
+		}
+	}
 }
 </style>
